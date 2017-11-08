f67152e0f0e9422dd0bec65ffeee4038705beab6
Develop Zakk <DevelopZakk@outlook.com>
2017-11-08 15:33:15 +0800

    update globalasax

--- MVCLifeCycle/Global.asax
+++ MVCLifeCycle/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCLifeCycle.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCLifeCycle.Global" Language="C#" %>
--- MVCLifeCycle/Global.asax.cs
+++ MVCLifeCycle/Global.asax.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -8,14 +7,39 @@ using System.Web.Routing;
 
 namespace MVCLifeCycle
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class Global : HttpApplication
     {
-        protected void Application_Start()
+        protected void Application_Start(object sender, EventArgs e)
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            Debug.WriteLine("****************Application Down***********");
         }
     }
 }
