b93e03ddf5457045a2d0b2ce08c95b551e321a4c
Develop Zakk <DevelopZakk@outlook.com>
2017-11-08 15:26:21 +0800

    update route config

--- MVCLifeCycle/App_Start/RouteConfig.cs
+++ MVCLifeCycle/App_Start/RouteConfig.cs
@@ -13,11 +13,16 @@ namespace MVCLifeCycle
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+            Route myRoute = new Route("{controller}/{action}/{id}",
+                new RouteValueDictionary(){{"controller","home"},{"action","Index"},{"id","1"}},
+                new MvcRouteHandler());
+            routes.Add(myRoute);
         }
     }
 }
