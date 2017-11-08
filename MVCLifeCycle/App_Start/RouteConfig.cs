3456d6aef8423c985e68bc368e7f906357722bb8
Develop Zakk <DevelopZakk@outlook.com>
2017-11-08 16:53:32 +0800

    Add custom httphandler

--- MVCLifeCycle/App_Start/RouteConfig.cs
+++ MVCLifeCycle/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MVCLifeCycle.Handlers;
 
 namespace MVCLifeCycle
 {
@@ -11,18 +12,29 @@ namespace MVCLifeCycle
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
+            routes.Add(new Route("home/test", new SampleRouteHandler()));
 
-            Route myRoute = new Route("{controller}/{action}/{id}",
-                new RouteValueDictionary(){{"controller","home"},{"action","Index"},{"id","1"}},
-                new MvcRouteHandler());
-            routes.Add(myRoute);
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            //Route myRoute = new Route("{controller}/{action}/{id}",
+            //    new RouteValueDictionary(){{"controller","home"},{"action","Index"},{"id","1"}},
+            //    new MvcRouteHandler());
+            //routes.Add(myRoute);
+        }
+    }
+
+    public class SampleRouteHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new SampleHandler();
         }
     }
 }
---
+++ MVCLifeCycle/Handlers/SampleHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+
+namespace MVCLifeCycle.Handlers
+{
+    public class SampleHandler: IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("<p> this is a test ... from sample handler </p>");
+        }
+
+        public bool IsReusable => false;
+    }
+}
