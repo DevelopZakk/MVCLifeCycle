ed437c49d55aeafcccb5b703d0857ae3ba46bcbd
Develop Zakk <DevelopZakk@outlook.com>
2017-11-08 16:01:28 +0800

    HttpModule

--- MVCLifeCycle/Global.asax.cs
+++ MVCLifeCycle/Global.asax.cs
@@ -4,11 +4,20 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MVCLifeCycle;
+
+//[assembly:PreApplicationStartMethod(typeof(Global),"Register")]
 
 namespace MVCLifeCycle
 {
     public class Global : HttpApplication
     {
+
+        public static void Register()
+        {
+            HttpApplication.RegisterModule(typeof(LogModule));
+        }
+
         protected void Application_Start(object sender, EventArgs e)
         {
             AreaRegistration.RegisterAllAreas();
@@ -19,22 +28,38 @@ namespace MVCLifeCycle
 
         protected void Session_Start(object sender, EventArgs e)
         {
+            Debug.WriteLine("Session Start");
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
+
+            Debug.WriteLine("Begin Request");
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
         {
+            Debug.WriteLine("Authenticate Request");
         }
 
         protected void Application_Error(object sender, EventArgs e)
         {
+            Debug.WriteLine("Application Error");
+        }
+
+        protected void Application_ResolveRequestCache(object sender, EventArgs e)
+        {
+            Debug.WriteLine("URL routing Module, resolve request cache");
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("End Request");
         }
 
         protected void Session_End(object sender, EventArgs e)
         {
+            Debug.WriteLine("Session End");
         }
 
         protected void Application_End(object sender, EventArgs e)
---
+++ MVCLifeCycle/LogModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+
+namespace MVCLifeCycle
+{
+    public class LogModule:IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            //throw new System.NotImplementedException();
+            Debug.WriteLine("~~~~~~~~~~~~");
+            context.BeginRequest +=
+                (new EventHandler(this.Application_BeginRequest));
+            context.EndRequest +=
+                (new EventHandler(this.Application_EndRequest));
+        }
+
+        private void Application_EndRequest(object sender, EventArgs e)
+        {
+            var context =((HttpApplication) sender).Context;
+            Debug.WriteLine($"{context.Request.Path}");
+        }
+
+        private void Application_BeginRequest(object sender, EventArgs e)
+        {
+        }
+
+        public void Dispose()
+        {
+            //throw new System.NotImplementedException();
+        }
+    }
+}
